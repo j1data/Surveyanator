07e7ba8eed6bbba39f5fa00fae3acf21f0411116
j1data123 <93102139+j1data@users.noreply.github.com>
2021-10-28 11:33:34 -0500

    Var "Mach" changed to Mach_val
    Mach the function and Mach the variable were conflicting, so I changed the variable to Mach_val

---
+++ Reformatted_JP_Script.m
@@ -0,0 +1,9 @@
+clear,clc
+%AerE 261
+%Jr.JPL Blake, Ellie, Jeremy, Justin, Nicole
+%Surveyanator
+%scaling factor 0.28:1 to the bonanza
+%based of Beechcraft Bonanza and Cessna 172
+
+inputs = GetGoogleSpreadsheet('1mX9oFI3Zd5SyJR2twwYRWJ417U7gUv60qco82aeOLdE');
+
